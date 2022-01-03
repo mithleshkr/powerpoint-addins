8286b5958689ea99846505d3f3e654212f2033d4
mithleshkr <79351404+mithleshkr@users.noreply.github.com>
2022-01-03 10:58:56 +0530

    Create line.vb

---
+++ line.vb
@@ -0,0 +1,297 @@
+Sub DetermineActiveShape()
+'PURPOSE: Determine the currently selected shape in PowerPoint
+'SOURCE: www.TheSpreadsheetGuru.com/the-code-vault
+
+Dim ActiveShape As Shape
+Dim shp As Shape
+
+'Determine Which Shape is Active
+  If ActiveWindow.Selection.Type = ppSelectionShapes Then
+    'Loop in case multiples shapes selected
+       For Each shp In ActiveWindow.Selection.ShapeRange
+         'ActiveShape is first shape selected
+            Set ActiveShape = shp
+            Exit For
+       Next shp
+  Else
+    MsgBox "There is no shape currently selected!", vbExclamation, "No Shape Found"
+  End If
+
+'Do Something with the ActiveShape
+
+ With ActiveShape
+    .Line.ForeColor.RGB = RGB(64, 224, 208)
+    End With
+
+End Sub
+
+Sub DetermineActiveShape1()
+'PURPOSE: Determine the currently selected shape in PowerPoint
+'SOURCE: www.TheSpreadsheetGuru.com/the-code-vault
+
+Dim ActiveShape As Shape
+Dim shp As Shape
+
+'Determine Which Shape is Active
+  If ActiveWindow.Selection.Type = ppSelectionShapes Then
+    'Loop in case multiples shapes selected
+       For Each shp In ActiveWindow.Selection.ShapeRange
+         'ActiveShape is first shape selected
+            Set ActiveShape = shp
+            Exit For
+       Next shp
+  Else
+    MsgBox "There is no shape currently selected!", vbExclamation, "No Shape Found"
+  End If
+
+'Do Something with the ActiveShape
+
+ With ActiveShape
+    .Line.ForeColor.RGB = RGB(128, 0, 128)
+    End With
+
+End Sub
+
+Sub DetermineActiveShape2()
+'PURPOSE: Determine the currently selected shape in PowerPoint
+'SOURCE: www.TheSpreadsheetGuru.com/the-code-vault
+
+Dim ActiveShape As Shape
+Dim shp As Shape
+
+'Determine Which Shape is Active
+  If ActiveWindow.Selection.Type = ppSelectionShapes Then
+    'Loop in case multiples shapes selected
+       For Each shp In ActiveWindow.Selection.ShapeRange
+         'ActiveShape is first shape selected
+            Set ActiveShape = shp
+            Exit For
+       Next shp
+  Else
+    MsgBox "There is no shape currently selected!", vbExclamation, "No Shape Found"
+  End If
+
+'Do Something with the ActiveShape
+
+ With ActiveShape
+    .Line.ForeColor.RGB = RGB(255, 0, 0)
+    End With
+
+End Sub
+
+Sub DetermineActiveShape3()
+'PURPOSE: Determine the currently selected shape in PowerPoint
+'SOURCE: www.TheSpreadsheetGuru.com/the-code-vault
+
+Dim ActiveShape As Shape
+Dim shp As Shape
+
+'Determine Which Shape is Active
+  If ActiveWindow.Selection.Type = ppSelectionShapes Then
+    'Loop in case multiples shapes selected
+       For Each shp In ActiveWindow.Selection.ShapeRange
+         'ActiveShape is first shape selected
+            Set ActiveShape = shp
+            Exit For
+       Next shp
+  Else
+    MsgBox "There is no shape currently selected!", vbExclamation, "No Shape Found"
+  End If
+
+'Do Something with the ActiveShape
+
+ With ActiveShape
+    .Line.ForeColor.RGB = RGB(255, 165, 0)
+    End With
+
+End Sub
+
+Sub DetermineActiveShape4()
+'PURPOSE: Determine the currently selected shape in PowerPoint
+'SOURCE: www.TheSpreadsheetGuru.com/the-code-vault
+
+Dim ActiveShape As Shape
+Dim shp As Shape
+
+'Determine Which Shape is Active
+  If ActiveWindow.Selection.Type = ppSelectionShapes Then
+    'Loop in case multiples shapes selected
+       For Each shp In ActiveWindow.Selection.ShapeRange
+         'ActiveShape is first shape selected
+            Set ActiveShape = shp
+            Exit For
+       Next shp
+  Else
+    MsgBox "There is no shape currently selected!", vbExclamation, "No Shape Found"
+  End If
+
+'Do Something with the ActiveShape
+
+ With ActiveShape
+    .Line.ForeColor.RGB = RGB(255, 192, 203)
+    End With
+
+End Sub
+
+Sub DetermineActiveShape5()
+'PURPOSE: Determine the currently selected shape in PowerPoint
+'SOURCE: www.TheSpreadsheetGuru.com/the-code-vault
+
+Dim ActiveShape As Shape
+Dim shp As Shape
+
+'Determine Which Shape is Active
+  If ActiveWindow.Selection.Type = ppSelectionShapes Then
+    'Loop in case multiples shapes selected
+       For Each shp In ActiveWindow.Selection.ShapeRange
+         'ActiveShape is first shape selected
+            Set ActiveShape = shp
+            Exit For
+       Next shp
+  Else
+    MsgBox "There is no shape currently selected!", vbExclamation, "No Shape Found"
+  End If
+
+'Do Something with the ActiveShape
+
+ With ActiveShape
+    .Line.ForeColor.RGB = RGB(128, 128, 128)
+    End With
+
+End Sub
+
+Sub DetermineActiveShape6()
+'PURPOSE: Determine the currently selected shape in PowerPoint
+'SOURCE: www.TheSpreadsheetGuru.com/the-code-vault
+
+Dim ActiveShape As Shape
+Dim shp As Shape
+
+'Determine Which Shape is Active
+  If ActiveWindow.Selection.Type = ppSelectionShapes Then
+    'Loop in case multiples shapes selected
+       For Each shp In ActiveWindow.Selection.ShapeRange
+         'ActiveShape is first shape selected
+            Set ActiveShape = shp
+            Exit For
+       Next shp
+  Else
+    MsgBox "There is no shape currently selected!", vbExclamation, "No Shape Found"
+  End If
+
+'Do Something with the ActiveShape
+
+ With ActiveShape
+    .Line.ForeColor.RGB = RGB(0, 0, 0)
+    End With
+
+End Sub
+
+
+Sub DetermineActiveShape7()
+'PURPOSE: Determine the currently selected shape in PowerPoint
+'SOURCE: www.TheSpreadsheetGuru.com/the-code-vault
+
+Dim ActiveShape As Shape
+Dim shp As Shape
+
+'Determine Which Shape is Active
+  If ActiveWindow.Selection.Type = ppSelectionShapes Then
+    'Loop in case multiples shapes selected
+       For Each shp In ActiveWindow.Selection.ShapeRange
+         'ActiveShape is first shape selected
+            Set ActiveShape = shp
+            Exit For
+       Next shp
+  Else
+    MsgBox "There is no shape currently selected!", vbExclamation, "No Shape Found"
+  End If
+
+'Do Something with the ActiveShape
+
+ With ActiveShape
+    .Line.ForeColor.RGB = RGB(165, 42, 42)
+    End With
+
+End Sub
+
+Sub DetermineActiveShape8()
+'PURPOSE: Determine the currently selected shape in PowerPoint
+'SOURCE: www.TheSpreadsheetGuru.com/the-code-vault
+
+Dim ActiveShape As Shape
+Dim shp As Shape
+
+'Determine Which Shape is Active
+  If ActiveWindow.Selection.Type = ppSelectionShapes Then
+    'Loop in case multiples shapes selected
+       For Each shp In ActiveWindow.Selection.ShapeRange
+         'ActiveShape is first shape selected
+            Set ActiveShape = shp
+            Exit For
+       Next shp
+  Else
+    MsgBox "There is no shape currently selected!", vbExclamation, "No Shape Found"
+  End If
+
+'Do Something with the ActiveShape
+
+ With ActiveShape
+    .Line.ForeColor.RGB = RGB(0, 0, 139)
+    End With
+
+End Sub
+
+Sub DetermineActiveShape9()
+'PURPOSE: Determine the currently selected shape in PowerPoint
+'SOURCE: www.TheSpreadsheetGuru.com/the-code-vault
+
+Dim ActiveShape As Shape
+Dim shp As Shape
+
+'Determine Which Shape is Active
+  If ActiveWindow.Selection.Type = ppSelectionShapes Then
+    'Loop in case multiples shapes selected
+       For Each shp In ActiveWindow.Selection.ShapeRange
+         'ActiveShape is first shape selected
+            Set ActiveShape = shp
+            Exit For
+       Next shp
+  Else
+    MsgBox "There is no shape currently selected!", vbExclamation, "No Shape Found"
+  End If
+
+'Do Something with the ActiveShape
+
+ With ActiveShape
+    .Line.ForeColor.RGB = RGB(173, 216, 230)
+    End With
+
+End Sub
+
+Sub DetermineActiveShape10()
+'PURPOSE: Determine the currently selected shape in PowerPoint
+'SOURCE: www.TheSpreadsheetGuru.com/the-code-vault
+
+Dim ActiveShape As Shape
+Dim shp As Shape
+
+'Determine Which Shape is Active
+  If ActiveWindow.Selection.Type = ppSelectionShapes Then
+    'Loop in case multiples shapes selected
+       For Each shp In ActiveWindow.Selection.ShapeRange
+         'ActiveShape is first shape selected
+            Set ActiveShape = shp
+            Exit For
+       Next shp
+  Else
+    MsgBox "There is no shape currently selected!", vbExclamation, "No Shape Found"
+  End If
+
+'Do Something with the ActiveShape
+
+ With ActiveShape
+    .Line.ForeColor.RGB = RGB(173, 255, 47)
+    End With
+
+End Sub
